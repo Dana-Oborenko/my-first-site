21aee7095214ddbf4d50d8bfa1d61a288963284c
Dana <danulja2005@inbox.lv>
2021-12-08 12:14:42 +0200

    Design adding

--- script.js
+++ script.js
@@ -62,3 +62,47 @@ prev.addEventListener('click', () =>{
 
     images[activeIndex].classList.add('active')
 })*/
+
+const commetBox = document.querySelector('.js-comments')
+let html = ''
+
+const addHtml = (name, email, body) => {
+    html = html + `
+    <div class="comment>
+        <h4 class="name"> Vārds:${name}<h4/>
+        <span class="email"> E-pasts:${email}<span/>
+        <p class="text">${body}<p/>
+    <div/>
+    `
+}
+const getComments = async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+    const comments = await data.json()
+    
+    comments.forEach((comment) => {
+        const {name, email, body} = comment
+        addHtml (name, email, body)
+
+        
+    });
+
+    commentBox.innerHTML = html
+
+}
+
+getComments()
+
+const form = document.querySelector('.js-form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault
+
+    const name = document.querySelector('.js-form-text').value
+    const email = document.querySelector('.js-form-email').value
+    const body = document.querySelector('.js-form-body').value
+
+    addHtml (name, email, body)
+    commentBox.innerHTML = html
+})
+
+
