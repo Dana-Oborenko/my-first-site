4b25660c90a66e52649226734fa32a1dbf2d7023
Dana <danulja2005@inbox.lv>
2021-12-09 12:28:43 +0200

    design upgrade

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-let images = document.querySelectorAll('.slider__image')
-let next = document.querySelector('.slider__next')
-let prev = document.querySelector('.slider__prev')
+const images = document.querySelectorAll('.slider__image')
+const dots = document.querySelectorAll('.slider__dot')
+const next = document.querySelector('.slider__next')
+const prev = document.querySelector('.slider__prev')
 let activeIndex = 0
 
+dots.forEach(function (dot, i) {
+    dot.addEventListener('click', () => {
+        images.forEach((image) => {
+            image.classList.remove('active')
+        })
+        dots.forEach((dot) => {
+            dot.classList.remove('active')
+        })
+
+        activeIndex = i
+        
+        images[activeIndex].classList.add('active')
+        dots[activeIndex].classList.add('active')
+    })
+})
+
 next.addEventListener('click', () => {
     images.forEach((image) => {
         image.classList.remove('active')
+    })
+    dots.forEach((dot) => {
+        dot.classList.remove('active')
     })
 
     if (activeIndex === images.length - 1) {
@@ -13,13 +33,17 @@ next.addEventListener('click', () => {
     } else {
         activeIndex = activeIndex + 1
     }
-
-     images[activeIndex].classList.add('active')
+    
+    images[activeIndex].classList.add('active')
+    dots[activeIndex].classList.add('active')
 })
 
 prev.addEventListener('click', () =>{
     images.forEach((image) => {
         image.classList.remove('active')
+    })
+    dots.forEach((dot) => {
+        dot.classList.remove('active')
     })
 
     if (activeIndex === 0) {
@@ -29,65 +53,31 @@ prev.addEventListener('click', () =>{
     }
 
     images[activeIndex].classList.add('active')
+    dots[activeIndex].classList.add('active')
 })
 
-/*images = document.querySelectorAll('.js-image')
-next = document.querySelector('.js-next')
-prev = document.querySelector('.js-prev')
-
-next.addEventListener('click', () => {
-    images.forEach((image) => {
-        image.classList.remove('active')
-    })
-
-    if (activeIndex === images.length - 1) {
-        activeIndex = 0
-    } else {
-        activeIndex = activeIndex + 1
-    }
-
-     images[activeIndex].classList.add('active')
-})
-
-prev.addEventListener('click', () =>{
-    images.forEach((image) => {
-        image.classList.remove('active')
-    })
-
-    if (activeIndex === 0) {
-         activeIndex = images.length - 1
-    } else {
-        activeIndex = activeIndex - 1
-    }
-
-    images[activeIndex].classList.add('active')
-})*/
-
-const commetBox = document.querySelector('.js-comments')
+const commentBox = document.querySelector('.js-comments')
 let html = ''
 
 const addHtml = (name, email, body) => {
-    html = html + `
-    <div class="comment>
-        <h4 class="name"> Vārds:${name}<h4/>
-        <span class="email"> E-pasts:${email}<span/>
-        <p class="text">${body}<p/>
-    <div/>
-    `
+    html = html +
+    `<div class="comment">
+        <h4 class="name">Vārds: ${name}</h4>
+        <span class="email">E-pasts: ${email}</span>
+        <p class="body">${body}</p>
+    </div>`
 }
+
 const getComments = async () => {
     const data = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
     const comments = await data.json()
     
     comments.forEach((comment) => {
         const {name, email, body} = comment
-        addHtml (name, email, body)
-
-        
-    });
+        addHtml(name, email, body)
+    })
 
     commentBox.innerHTML = html
-
 }
 
 getComments()
@@ -95,14 +85,44 @@ getComments()
 const form = document.querySelector('.js-form')
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault
+    e.preventDefault()
+    
+    const name = document.querySelector('.js-form-text')
+    const email = document.querySelector('.js-form-email')
+    const body = document.querySelector('.js-form-body')
 
-    const name = document.querySelector('.js-form-text').value
-    const email = document.querySelector('.js-form-email').value
-    const body = document.querySelector('.js-form-body').value
+    if (name.value === '') {
+        alert('Lūdzu, ievadiet savu vārdu!')
+        return
+    }
+    if (email.value === '') {
+        alert('Lūdzu, ievadiet savu e-pastu!')
+        return
+    }
+    if (body.value === '') {
+        alert('Lūdzu, ievadiet sava komentāra tekstu!')
+        return
+    }
 
-    addHtml (name, email, body)
+    addHtml(name.value, email.value, body.value.trim())
     commentBox.innerHTML = html
+
+    name.value = ''
+    email.value = ''
+    body.value = ''
 })
 
+const toTopButton = document.querySelector('#go-to-top')
 
+window.onscroll = () => {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        toTopButton.style.display = 'block'
+    } else {
+        toTopButton.style.display = 'none'
+    }
+}
+
+toTopButton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
